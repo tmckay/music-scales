bc28aa556f2e1aeba640a3b9cf2776dc6f585b7f
Taylor McKay <taylormckay@gmail.com>
2021-07-29 22:01:01 -0700

    Add minor scales

--- scales.tcl
+++ scales.tcl
@@ -3,3 +3,5 @@
 #   scales.tcl c major -> c d e f g a b
 
 set "scale_intervals(major)" "w w h w w w h"
+set "scale_interval(minor)" "w h w w h w w"
+set "scale_interval(harmonic_minor)" "w h w w h w h h"
