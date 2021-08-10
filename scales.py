f199c0a62dada5a1cb17813de3494f9c70858f48
Taylor McKay <taylormckay@gmail.com>
2021-08-09 22:53:57 -0700

    Add start of Python version of scales

---
+++ scales.py
@@ -0,0 +1,16 @@
+NOTES = 'c c-sharp/d-flat'
+
+class Note:
+    def __init__(self):
+        pass
+
+
+class Scale:
+    def __init__(self, intervals):
+        self._intervals = intervals 
+
+    def in_key(self, key):
+        pass
+
+
+major = Scale('w w h w w w h')
