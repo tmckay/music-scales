9949c5e8465686f7df24fbb6a372a69134e8b384
Taylor McKay <taylormckay@gmail.com>
2021-08-10 21:44:48 -0700

    Generate note from scale interval

--- scales.py
+++ scales.py
@@ -1,4 +1,5 @@
-NOTES = 'c c-sharp/d-flat'
+NOTES = 'c c# d d# e f f# g g# a a# b'
+
 
 class Note:
     def __init__(self):
@@ -7,10 +8,32 @@ class Note:
 
 class Scale:
     def __init__(self, intervals):
-        self._intervals = intervals 
+        self._intervals = intervals.split()
 
     def in_key(self, key):
-        pass
+        notes = NOTES.split()
+
+        if key not in notes:
+            raise ValueError(f'"{key}" is not a valid key')
+
+        idx = notes.index(key)
+
+        key_notes = []
+
+        for step in self._intervals:
+            key_notes.append(notes[idx])
+
+            if step == 'w':
+                idx += 2
+            elif step == 'h':
+                idx += 1
+            else:
+                raise ValueError(f'Incorrect value "{step}" for scale interval')
+
+            idx = idx % len(notes)
+
+        return key_notes
 
 
 major = Scale('w w h w w w h')
+print(major.in_key('c'))
