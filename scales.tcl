19ce286ccf44b7ab64c08d3d8f7cabdcb2f7b67f
Taylor McKay <taylormckay@gmail.com>
2021-08-23 20:55:53 -0700

    Remove Tcl version of scales

--- scales.tcl
+++
@@ -1,9 +0,0 @@
-# Generate scale based on root and scale name
-# Example scale:
-#   scales.tcl c major -> c d e f g a b
-
-set "scale_intervals(major)" "w w h w w w h"
-set "scale_interval(minor)" "w h w w h w w"
-set "scale_interval(melodic_minor)" "w h w w w w h"
-set "scale_interval(harmonic_minor)" "w h w w h w h h"
-set "scale_interval(diminished)" "w h w h w h w h"
