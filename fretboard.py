bad297310bda7008f8eb9e0e61a42ae125873453
Taylor McKay <taylormckay@gmail.com>
2021-08-30 22:13:12 -0700

    Small factoring of fretboard

--- fretboard.py
+++ fretboard.py
@@ -11,46 +11,46 @@ class Fretboard:
         self.tuning = tuning
         self.number_frets = number_frets
 
+    @staticmethod
+    def find_fret_for_note(open_note, target_note):
+        notes = NOTES.split()
+        open_note_idx = -1
+        for idx, note in enumerate(notes):
+            sub_notes = note.split('/')
+
+            if open_note in sub_notes:
+                open_note_idx = idx
+
+        fret = None
+        fret_idx = 0
+        note_idx = open_note_idx
+        while fret is None:
+            if target_note in notes[note_idx].split('/'):
+                fret = fret_idx
+            else:
+                note_idx += 1
+                fret_idx += 1
+
+            note_idx = note_idx % len(notes)
+        return fret
+
+    @staticmethod
+    def are_frets_in_limit(fret_a, fret_b, limit):
+        return abs(fret_b - fret_a) <= limit
+
     def find_scale(self, scale, starting_string=0, fret_reach_limit=3):
         """starting_string is index of string in tuning"""
-        def find_fret_for_note(open_note, target_note):
-            #print(f'looking for {target_note} on open string {open_note}')
-            notes = NOTES.split()
-            open_note_idx = -1
-            for idx, note in enumerate(notes):
-                sub_notes = note.split('/')
-
-                if open_note in sub_notes:
-                    open_note_idx = idx
-
-            fret = None
-            fret_idx = 0
-            note_idx = open_note_idx
-            while fret is None:
-                if target_note in notes[note_idx].split('/'):
-                    fret = fret_idx
-                else:
-                    note_idx += 1
-                    fret_idx += 1
-
-                note_idx = note_idx % len(notes)
-            return fret
-
-        def are_frets_in_limit(fret_a, fret_b, limit):
-            return math.abs(fret_b - fret_a) <= limit
-
         frets = []
         queue = deque()
         for note in scale:
             note = note.split('/')[0]
-            #print(f'note in scale {note}')
             queue.append((starting_string, note))
             while len(queue) > 0:
                 target = queue.popleft()
-                result = find_fret_for_note(self.tuning[target[0]], target[1])
+                result = self.find_fret_for_note(self.tuning[target[0]], target[1])
 
                 if len(frets) > 0 and \
-                    (abs(result - frets[-1][1]) > fret_reach_limit or\
+                    (not self.are_frets_in_limit(result, frets[-1][1], fret_reach_limit) or\
                     abs(target[0] - frets[-1][0]) > 1):
                         queue.append((target[0] + 1, target[1]))
                 else:
