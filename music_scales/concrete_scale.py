f9f01d99fc72ae50be0f9a7f4daed68611d33d4f
Taylor McKay <taylormckay@gmail.com>
2021-09-19 20:11:16 -0700

    Draw note on fretboard

--- music_scales/concrete_scale.py
+++ music_scales/concrete_scale.py
@@ -1,4 +1,5 @@
 """Concrete scale on a fretboard"""
+from math import pi as M_PI
 from typing import List, Tuple
 
 import cairo
@@ -20,7 +21,7 @@ class ConcreteScale:
         self.name = name
         self.scale_def = scale_def
 
-    def as_image(self, width=800, height=200):
+    def as_image(self, width=800, height=800):
         """Return image of scale on a fretboard"""
         print('creating a fretboard image')
         with cairo.SVGSurface("concrete_scale.svg", width, height) as surface:
@@ -35,11 +36,17 @@ class ConcreteScale:
             # draw guitar strings
             context.set_source_rgb(0, 0, 0)
             context.set_line_width(0.02)
+            string_gap = 1 / 6.0
             for line_idx in range(6):
-                string_gap = 1 / 6.0
                 y_value = string_gap * line_idx + string_gap / 2
                 context.move_to(0, y_value)
                 context.line_to(1, y_value)
+            context.stroke()
+
+            # show dots on strings for scale
+            note_gap = 1 / 8.0
+            context.arc(note_gap, string_gap / 2, 0.05, 0.0, 2.0 * M_PI)
+            context.fill()
             context.stroke()
 
             # save image
