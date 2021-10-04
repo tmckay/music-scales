c6a44e982b4178947235088596c1edabc44896bf
Taylor McKay <taylormckay@gmail.com>
2021-10-03 21:03:13 -0700

    Factor drawing fretboard into separate module

--- music_scales/concrete_scale.py
+++ music_scales/concrete_scale.py
@@ -2,8 +2,7 @@
 from math import pi as M_PI
 from typing import List, Tuple
 
-import cairo
-
+from .fretboard_drawer import FretboardDrawer
 from .note import Note
 from .utils import to_ordinal
 
@@ -28,85 +27,23 @@ class ConcreteScale:
         """Returns a key that is path safe for saving images"""
         return str(self.key).split('/')[0]
 
-    def _get_blank_fretboard_image(self, width, height):
-        """Draws an image with an empty fretboard"""
-        surface = cairo.SVGSurface("concrete_scale.svg", width, height)
-        context = cairo.Context(surface)
-        context.scale(width, height)
-
-        # set background color
-        context.rectangle(0, 0, width, height)
-        context.set_source_rgb(1, 1, 1)
-        context.fill()
-
-        # draw guitar strings
-        context.set_source_rgb(0, 0, 0)
-        context.set_line_width(0.02)
-        string_gap = 1 / 6.0  # TODO Parametrize num of strings
-        for line_idx in range(6):
-            y_value = string_gap * line_idx + string_gap / 2
-            context.move_to(0, y_value)
-            context.line_to(1, y_value)
-        context.stroke()
-
-        # draw guitar frets
-        fret_gap = 1 / 6.0
-        for fret_idx in range(6):
-            x_value = fret_gap * fret_idx + fret_gap / 2
-            context.move_to(x_value, fret_gap / 2)
-            context.line_to(x_value, 1 - fret_gap / 2)
-        context.stroke()
-
-        return context, surface
-
     def as_image(self, width=800, height=800):
         """Return image of scale on a fretboard"""
 
-        def add_note_to_fretboard(string: int, fret: int, first_fret: int, root: bool = False):
-            note_gap = 1 / 6.0
-            string_depth = abs(string - 5)
-            x_coord = note_gap * 2 + (note_gap * (fret - first_fret))
-            string_gap = 1 / 6.0  # TODO Parametrize num of strings
-            y_coord = string_gap / 2 + string_gap * string_depth
-            context.arc(
-                x_coord,
-                y_coord,
-                0.05,
-                0.0,
-                2.0 * M_PI
-            )
-            context.fill()
-
-            # draw circle around root notes
-            if root:
-                context.set_line_width(0.01)
-                context.arc(
-                    x_coord,
-                    y_coord,
-                    0.07,
-                    0.0,
-                    2.0 * M_PI
-                )
-            context.stroke()
-
-        context, surface = self._get_blank_fretboard_image(width, height)
+        fretboard_drawer = FretboardDrawer(width, height)
 
         # show dots on strings for scale
         first_fret = self.scale_def[0][1]
         for idx, note in enumerate(self.scale_def):
             # label fret
             if idx == 0:
-                context.select_font_face('Sans')
-                context.set_font_size(0.05)
-                context.move_to(1 / 6.0 * 1.75, 0.06)
                 ordinal = to_ordinal(note[1])
-                context.show_text(f'{ordinal} fret')
-            add_note_to_fretboard(
+                fretboard_drawer.add_label(f'{ordinal} fret')
+                
+            fretboard_drawer.add_note(
                 note[0],
                 note[1],
                 first_fret,
                 idx in (0, len(self.scale_def)-1)
             )
-
-        # save image
-        surface.write_to_png(f'/images/{self.name}_{self.path_safe_key}.png')
+        fretboard_drawer.save(f'/images/{self.name}_{self.path_safe_key}.png')
---
+++ music_scales/fretboard_drawer.py
@@ -0,0 +1,103 @@
+"""Draws a fretboard image for adding notes"""
+from math import pi as M_PI
+
+import cairo
+
+class FretboardDrawer:
+    """Draws a base fretboard image on which notes can be added"""
+
+    def __init__(self,
+                 width=800, height=800,
+                 bgcolor=(1,1,1), fgcolor=(0,0,0),
+                 num_strings=6, num_frets=6):
+        self.width = width
+        self.height = height
+        self.bgcolor = bgcolor
+        self.fgcolor = fgcolor
+        self.num_strings = num_strings
+        self.num_frets = num_frets
+
+        self._draw_blank_fretboard()
+
+    def _draw_blank_fretboard(self):
+        """Draws an image with an empty fretboard"""
+        surface = cairo.SVGSurface(
+            'fretboard.svg',
+            self.width,
+            self.height
+        )
+        context = cairo.Context(surface)
+        context.scale(self.width, self.height)
+
+        # set background color
+        context.rectangle(0, 0, self.width, self.height)
+        context.set_source_rgb(*self.bgcolor)
+        context.fill()
+
+        # draw guitar strings
+        context.set_source_rgb(*self.fgcolor)
+        context.set_line_width(0.02)
+        string_gap = 1 / self.num_strings
+        for line_idx in range(self.num_strings):
+            y_value = string_gap * line_idx + string_gap / 2
+            context.move_to(0, y_value)
+            context.line_to(1, y_value)
+        context.stroke()
+
+        # draw guitar frets
+        fret_gap = 1 / self.num_frets
+        for fret_idx in range(self.num_frets):
+            x_value = fret_gap * fret_idx + fret_gap / 2
+            context.move_to(x_value, fret_gap / 2)
+            context.line_to(x_value, 1 - fret_gap / 2)
+        context.stroke()
+
+        self.context = context
+        self.surface = surface
+
+    def add_note(self, string: int, fret: int, first_fret: int, root: bool = False):
+        """
+        Args:
+            string: string number to draw note on
+            fret: fret number to draw note on
+            first fret: first fret of scale
+            root: whether it's a root note of scale
+        """
+        note_gap = 1 / self.num_frets
+        string_depth = abs(string - self.num_strings + 1)
+        x_coord = note_gap * 2 + (note_gap * (fret - first_fret))
+        string_gap = 1 / self.num_strings
+        y_coord = string_gap / 2 + string_gap * string_depth
+        self.context.arc(
+            x_coord,
+            y_coord,
+            0.05,
+            0.0,
+            2.0 * M_PI
+        )
+        self.context.fill()
+
+        # draw circle around root notes
+        if root:
+            self.context.set_line_width(0.01)
+            self.context.arc(
+                x_coord,
+                y_coord,
+                0.07,
+                0.0,
+                2.0 * M_PI
+            )
+        self.context.stroke()
+
+    def add_label(self, text):
+        self.context.select_font_face('Sans')
+        self.context.set_font_size(0.05)
+        self.context.move_to(1 / self.num_frets * 1.75, 0.06)
+        self.context.show_text(text)
+
+    def save(self, path):
+        # save image
+        self.surface.write_to_png(path)
+
+
+
