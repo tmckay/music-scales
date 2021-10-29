a09ccab6f874411828fb9fbd79e640ca230df1d1
Taylor McKay <taylormckay@gmail.com>
2021-10-28 21:30:02 -0700

    Add test for in_key on Scale class

--- music_scales/tests/test_scale.py
+++ music_scales/tests/test_scale.py
@@ -1,6 +1,18 @@
+from music_scales.note import Note
 from music_scales.scale import Scale
 
 
-def test_scale():
+def test_scale_repr():
     scale = Scale('foo_name', 'w w', 'bar_mode')
     assert repr(scale) == "Scale('foo_name', 'w w', 'bar_mode')"
+
+
+def test_scale_in_key():
+    scale = Scale('foo', 'w w w h 3h')
+    assert scale.in_key('c') == [
+        Note('c'),
+        Note('d'),
+        Note('e'),
+        Note('f#'),
+        Note('g')
+    ]
